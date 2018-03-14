68cdba21ebc207402d466abf8ff3574695d5d649
Caio Dias <kiobreno@gmail.com>
2018-03-13 20:29:59 -0400

    Delete LoginController class

--- Advanced-Course-Project/Controller/FetchController.swift
+++ Advanced-Course-Project/Controller/FetchController.swift
@@ -13,24 +13,81 @@ import AlamofireObjectMapper
 internal class FetchController {
     public static let shared: FetchController = FetchController()
     
+    private var token: String = ""
+    private let username: String = "prodigiadvancedcourse@gmail.com"
+    private let password: String = "prodigi123!@#"
+    private let loginURL: String = "https://www.googleapis.com/identitytoolkit/v3/relyingparty/verifyPassword?key=AIzaSyB8m7LmEtH4wNjFKEfKnaUcUUJVdp1Ntx4"
+    
     private init() {
         // Nothing :)
+    }
+    
+    public func doLogin(onSuccess: @escaping () -> Void, onFail: @escaping (Error) -> Void) {
+        guard let url = URL(string: loginURL) else {
+            print("Couldn't get job list")
+            return
+        }
+        
+        let httpBody = ["email": username, "password": password, "returnSecureToken" : "true"]
+        
+        guard let httpBodyData = try? JSONSerialization.data(withJSONObject: httpBody, options: []) else {
+            return
+        }
+        
+        var urlRequest = URLRequest(url: url)
+        urlRequest.httpMethod = "POST"
+        urlRequest.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        urlRequest.httpBody = httpBodyData
+        
+        Alamofire.request(urlRequest).validate().responseJSON { response in
+            switch response.result {
+            case .success:
+                if let data = response.data {
+                    guard
+                        let json = try? JSONSerialization.jsonObject(with: data, options: []),
+                        let dictionary = json as? [String: Any],
+                        let idToken = dictionary["idToken"] as? String
+                        else {
+                            print("Failed to get response data as json")
+                            return
+                    }
+                    
+                    self.token = idToken
+                    onSuccess()
+                }
+                
+            case .failure(let error):
+                print("ERROR: \(error.localizedDescription)")
+                
+                if let data = response.data, let utf8Text = String(data: data, encoding: .utf8) {
+                    print("Data: \(utf8Text)") // original server data as UTF8 string
+                }
+                
+                onFail(error)
+            }
+        }
     }
 }
 
 // MARK: Students
 
 extension FetchController {
-    internal func fetchAllStudents(onSuccess: @escaping () -> Void, onFail: @escaping (Error) -> Void) {
-        let url: String = "https://pro-digi-advanced.firebaseio.com/student/studentId.json?auth=eyJhbGciOiJSUzI1NiIsImtpZCI6IjQzZTFiMGIyOTllNDIxZjU2ZWI1YTQ2NjhkMWNmMjNmNGFjNjk2NGMifQ.eyJpc3MiOiJodHRwczovL3NlY3VyZXRva2VuLmdvb2dsZS5jb20vcHJvLWRpZ2ktYWR2YW5jZWQiLCJhdWQiOiJwcm8tZGlnaS1hZHZhbmNlZCIsImF1dGhfdGltZSI6MTUyMDk4MjM1MiwidXNlcl9pZCI6IklIbUtSRFM5SjJhMmhxWXNPa1dOZVpXQU5RMjIiLCJzdWIiOiJJSG1LUkRTOUoyYTJocVlzT2tXTmVaV0FOUTIyIiwiaWF0IjoxNTIwOTgyMzUyLCJleHAiOjE1MjA5ODU5NTIsImVtYWlsIjoicHJvZGlnaWFkdmFuY2VkY291cnNlQGdtYWlsLmNvbSIsImVtYWlsX3ZlcmlmaWVkIjpmYWxzZSwiZmlyZWJhc2UiOnsiaWRlbnRpdGllcyI6eyJlbWFpbCI6WyJwcm9kaWdpYWR2YW5jZWRjb3Vyc2VAZ21haWwuY29tIl19LCJzaWduX2luX3Byb3ZpZGVyIjoicGFzc3dvcmQifX0.EC2vB9Kl51rKdhv-c-GSXgfvkEOZdccX_mSvhKGTJf5wzMIIUUbv_J_2PUg9R-c79O8I0Ow_Mb06Xx8jNRTjbWtpBRKV8sJn-vvdVOpNf8DyYX47OxG0sxgCUJJh3Hd5Gz4AJayz2FeebHTazUIiNQMcZrbFWMLFPCVfs_sfNFyrCfZUQRXvogYqCevxddUFzCoO50JO2X0E5T8Zi-6dATxiS1OYq1wL87Qb25N73jeGVpwQoql53SXaAqi18B_sjcS1hcMtAfypPLwUL0gr9U90nngb003sbWcvqBj2QahtEbi_1tz4opqJ2W5R2LiwFIjdVSf2C2Tvb9fULBh5Sw"
-        
-        Alamofire.request(url).validate().responseObject { (response: DataResponse<Student>) in
-            switch response.result {
-            case .success(let value):
-                onSuccess()
-            case .failure(let error):
-                onFail(error)
+    internal func fetchAllStudents(onSuccess: @escaping ([Student]) -> Void, onFail: @escaping (Error) -> Void) {
+        self.doLogin(onSuccess: {
+            let url: String = "https://pro-digi-advanced.firebaseio.com/student/studentId.json?auth=\(self.token)"
+            
+            Alamofire.request(url).validate().responseArray { (response: DataResponse<[Student]>) in
+                switch response.result {
+                case .success(let value):
+                    onSuccess(value)
+                case .failure(let error):
+                    onFail(error)
+                }
             }
-        }
+        }, onFail: { error in
+            // TODO: Handle error scenario
+            print("ERROR: \(error.localizedDescription)")
+        })
+        
     }
 }
