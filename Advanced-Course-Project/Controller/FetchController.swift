948598a4651881e509fbc1d025c52b601fca9800
Caio Dias <kiobreno@gmail.com>
2018-03-13 20:23:08 -0400

    Add FetchController

---
+++ Advanced-Course-Project/Controller/FetchController.swift
@@ -0,0 +1,15 @@
+//
+//  FetchController.swift
+//  Advanced-Course-Project
+//
+//  Created by Caio Dias on 2018-03-13.
+//  Copyright © 2018 ProDigi-Development. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+import AlamofireObjectMapper
+
+internal class FetchController {
+    
+}
